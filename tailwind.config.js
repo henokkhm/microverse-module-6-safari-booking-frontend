914c9554899486f7f92ba4977b2289ba463e5ed2
HenaryKhuraijam <henarykhu123@gmail.com>
2024-01-19 21:04:34 +0530

    Install Configure TailwindForm

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssForms from '@tailwindcss/forms';
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    tailwindcssForms,
+  ],
 }

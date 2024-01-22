755f1a14df58e078a6ff7fbffee7f3a2bb8164ce
Henok Kirubel <henokhm2@gmail.com>
2024-01-22 23:52:42 +0300

    Make Sidebar responsive

--- src/pages/layout/Layout.js
+++ src/pages/layout/Layout.js
@@ -1,14 +1,55 @@
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
+
 import Sidebar from '../../components/Sidebar';
+import useWindowSize from '../../hooks/useWindowSize';
 
-const Layout = () => (
-  <>
-    <Sidebar />
+const MOBILE_BREAKPOINT = 720;
 
-    <main className="ml-96">
-      <Outlet />
-    </main>
-  </>
-);
+const Layout = () => {
+  const [sidebarMenuIsOpen, setSidebarMenuIsOpen] = useState(false);
+  const toggleSidebarMenu = () => setSidebarMenuIsOpen((prev) => !prev);
+
+  const { windowWidth } = useWindowSize();
+
+  // if (windowWidth > MOBILE_BREAKPOINT) {
+  //   /**
+  //    * Sidebar: shown (without regard to sidebarMenuIsOpen)
+  //    * Hamburger Menu Icon: hidden
+  //    * Overlay: hidden
+  //    * Main div: pushed to the right by 14rem
+  //    */
+  // } else {
+  //   if (sidebarMenuIsOpen) {
+  //     /**
+  //      * Sidebar: shown
+  //      * Hamburger Menu Icon: shown as close button
+  //      * Overlay: shown
+  //      * Main div: pushed to the right by 14rem
+  //      */
+  //   } else {
+  //     /**
+  //      * Sidebar: pushed to the left by 14rem,
+  //      * Hamburger Menu Icon: shown as open button
+  //      * Overlay: hidden
+  //      * Main div: original position
+  //      */
+  //   }
+  // }
+
+  return (
+    <div className="min-w-[380px] max-w-[1000px] mx-auto min-h-screen bg-white">
+      <Sidebar
+        sidebarMenuIsOpen={sidebarMenuIsOpen}
+        toggleSidebarMenu={toggleSidebarMenu}
+        breakpoint={MOBILE_BREAKPOINT}
+      />
+
+      <main className={`${((windowWidth > MOBILE_BREAKPOINT) || sidebarMenuIsOpen) ? 'ml-96' : 'ml-10'} `}>
+        <Outlet />
+      </main>
+    </div>
+  );
+};
 
 export default Layout;

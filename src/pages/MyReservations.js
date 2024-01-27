17b8b8a0e80a6c198d2aba64b6262295eea8d47f
HenaryKhuraijam <henarykhu123@gmail.com>
2024-01-28 04:10:03 +0530

    Refactor MyReservations.js Page

--- src/pages/MyReservations.js
+++ src/pages/MyReservations.js
@@ -1,5 +1,31 @@
-function MyReservations() {
-  return <div>My Reservations Page</div>;
-}
+import { FaRegSadTear } from 'react-icons/fa';
+import MyReservation from '../components/MyReservation';
+
+const MyReservations = () => {
+  const loading = false;
+  const reservationList = [
+    { id: 1, name: 'any', date: 'date' },
+    { id: 1, name: 'any', date: 'date' },
+    { id: 1, name: 'any', date: 'date' },
+  ];
+
+  return (
+    <>
+      <div className="flex flex-col justify-center h-screen w-full md:w-11/12 mx-auto ">
+        {loading && <span>...loading</span>}
+        {reservationList.length === 0 ? (
+          <div className="flex justify-center font-bold  text-6xl text-clrPrime">
+            No Record Found!
+            <FaRegSadTear className="spinner" />
+          </div>
+        ) : (
+          reservationList.map((reservation) => (
+            <MyReservation key={reservation.id} id={reservation.id} />
+          ))
+        )}
+      </div>
+    </>
+  );
+};
 
 export default MyReservations;

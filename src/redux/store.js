47ccaab5cc56bd254c1f7d994494c52d08ec23a8
HenaryKhuraijam <henarykhu123@gmail.com>
2024-01-28 07:20:06 +0530

    Update Store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import safarisReducer from './slices/safarisSlice';
 import loaderReducer from './slices/loaderSlice';
+import reservationReducer from './slices/reservationSlice';
 
 const rootReducer = {
   safaris: safarisReducer,
   loader: loaderReducer,
+  reservations: reservationReducer,
 };
 
 const store = configureStore({

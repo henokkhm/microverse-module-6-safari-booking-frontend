05fd78fbb9a3c6a129e18fc2d2d938b4b53073a0
Wils-FOURR-speed <124159862+Wils-FOURR-speed@users.noreply.github.com>
2024-01-30 03:11:57 +0100

    Add delete Safaris page

--- src/pages/DeleteSafaris.js
+++ src/pages/DeleteSafaris.js
@@ -1,26 +1,24 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-
 function DeleteSafariPage() {
   const [safariId, setSafariId] = useState('');
- 
+
   const deleteSafari = async () => {
-     try {
-       await axios.delete(`http://127.0.0.1:3000/safaris/${safariId}`);
-       alert('Safari deleted successfully!');
-     } catch (error) {
-       console.error("Failed to delete safari", error);
-     }
+    try {
+      await axios.delete(`http://127.0.0.1:3000/safaris/${safariId}`);
+      alert('Safari deleted successfully!');
+    } catch (error) {
+      console.error('Failed to delete safari', error);
+    }
   };
- 
+
   return (
-     <div>
-       <input type="text" value={safariId} onChange={e => setSafariId(e.target.value)} />
-       <button onClick={deleteSafari}>Delete Safari</button>
-     </div>
+    <div>
+      <input type="text" value={safariId} onChange={(e) => setSafariId(e.target.value)} />
+      <button type="button" onClick={deleteSafari}>Delete Safari</button>
+    </div>
   );
- }
- 
- export default DeleteSafariPage;
- 
+}
+
+export default DeleteSafariPage;

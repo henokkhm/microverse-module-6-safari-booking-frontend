61e445cd66ccfeef0d136f37ed8c9b2eaf61d224
Henok Kirubel <henokhm2@gmail.com>
2024-01-31 10:31:09 +0300

    Add SafariCard component

---
+++ src/components/SafariCard.js
@@ -0,0 +1,49 @@
+import PropTypes from 'prop-types';
+
+import facebookIcon from '../assets/icons/socials/facebook_2.svg';
+import twitterIcon from '../assets/icons/socials/twitter_2.svg';
+import instagramIcon from '../assets/icons/socials/instagram.svg';
+
+function SafariCard({ coverImageURL, name, description }) {
+  const shortDescription = description.split('\n\n')[0];
+
+  return (
+    <div className="flex flex-col gap-4 text-center items-center max-w-96">
+      <div>
+        <img src={`/safaris/${coverImageURL}`} alt={`photograph of ${name}`} />
+      </div>
+      <div className="pt-2 pb-8">
+        <h3 className="text-xl uppercase font-bold py-6">{name}</h3>
+        <div className="border-b-4 mb-6 mx-auto border-dotted border-gray-200 w-24" />
+        <p className="text-gray-400 pb-6 leading-7">{shortDescription}</p>
+        <div className="flex gap-3 justify-center items-center">
+          <a href="/">
+            <img
+              className="social-icon"
+              src={facebookIcon}
+              alt="facebook icon"
+            />
+          </a>
+          <a href="/">
+            <img className="social-icon" src={twitterIcon} alt="twitter icon" />
+          </a>
+          <a href="/">
+            <img
+              className="social-icon"
+              src={instagramIcon}
+              alt="instagram icon"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+SafariCard.propTypes = {
+  coverImageURL: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
+
+export default SafariCard;

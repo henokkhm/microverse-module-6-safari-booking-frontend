8645441aa5a753daad32091f46fadb520508ddf3
Henok Kirubel <henokhm2@gmail.com>
2024-02-01 22:47:33 +0300

    Fix login bug

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,18 +1,21 @@
 import LoginForm from '../components/LoginForm';
+import splashScreenImg from '../assets/logo.png';
 
 function Login() {
   return (
-    <div className="flex flex-col bg-[url('./assets/safari-a.jpg')] w-full bg-cover shadow-md items-center justify-center border-solid border p-4 sm:w-[600px]">
-      <p className="text-md text-red-600 text-center mb-3" id="msg" />
-      <h2>Sign in to your Safari account</h2>
-      <LoginForm />
-      <div className="link-cus">
-        Don&apos;t have an account?
-        <a href="/register" className="link-cus-title">
-          Sign up
-        </a>
+    <main className="page-container">
+      <div className="flex flex-col bg-st-green-50 gap-8 w-full bg-cover p-12 sm:w-[560px]">
+        <img className="h-80" src={splashScreenImg} alt="safari trek logo" />
+        <h2 className="text-3xl text-left font-bold pt-16">Please sign in to your account</h2>
+        <LoginForm />
+        <div className="link-cus flex gap-3 justify-center">
+          Don&apos;t have an account?
+          <a href="/register" className="link-cus-title">
+            Sign up
+          </a>
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
 
